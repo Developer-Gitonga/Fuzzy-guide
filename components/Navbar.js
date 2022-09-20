486a90c143bcaa29181d9159899bbbbbbb39422c
Develper-Gitonga <aizensalim52@gmail.com>
2022-09-20 13:19:48 +0300

    ch;boilerplate series

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,16 @@
 import Link from "next/link"
-import Image from "next/image";
 
 const Navbar = () => {
-    return (
-        <div className="navbar">
+    return (   
+            <div className="navbar">
             <div className="logo">
-            <img src="/logo.jpg" width={208} height={177} />
+                <img src="/logo.jpg" width={208} height={177} />
             </div>
             <Link href="/">Home</Link>
             <Link href="/profile">Profile</Link>
             <Link href="/about">About</Link>
-        </div>
+            </div>
+        
     );
 };
 
