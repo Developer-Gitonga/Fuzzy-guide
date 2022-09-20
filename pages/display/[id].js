8f99403cffebcd183c1690cf26c038c60c9b1c4f
Develper-Gitonga <aizensalim52@gmail.com>
2022-09-20 14:07:01 +0300

    ch;boilerplate series

--- pages/display/[id].js
+++ pages/display/[id].js
@@ -1,15 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
 import { data } from '../../../lib/data'
 
 const Display = ({item}) => {
-return (
+return (<>
     <div>
         <h1>{item?.id}</h1>
         <h1>{item?.name}</h1>
-        <Image src={`/${item?.image}`} alt="Vercel Logo" width={400} height={400} />
+        < Image src={`/${item.image}`} alt="levi.jpeg" width={400} height={400} />
         <p>{item?.description}</p>
                             
-    </div>
+    </div></>
 )
 }
 
@@ -18,7 +19,10 @@ export default Display
 export const getStaticPaths = () => {
     const paths = data.map(item => ({
         params: {
-            id: String(item.id)
+            id: String(item.id),
+            name: String(item.name),
+            Image: (item.image),
+            description: String(item.description)
         }
     }))
     return {
@@ -37,8 +41,8 @@ export const getStaticProps = ({params}) => {
             item: {
                 id: params.id,
                 // name: params.name,
-                // // image: params.image,
-                // description: params.description
+                // image: params.image,
+                // description: params.description,
 
             }
         }
